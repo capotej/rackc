0e95914229d4e907909659ebe008b775e3e92e28
Julio Capote <jcapote@gmail.com>
2011-09-14 17:56:56 -0700

    works

---
+++ config.ru
@@ -0,0 +1,5 @@
+require 'rack'
+
+run lambda { |env| 
+  [200, {'Content-Type'=>'text/plain'}, StringIO.new("Hello World!\n")] 
+}
---
+++ ext/rackc.c
@@ -0,0 +1,28 @@
+#include "ruby.h"
+
+VALUE rackc = Qnil;
+
+void Init_rackc();
+
+VALUE method_rackc_init(VALUE self);
+
+void Init_rackc() {
+  rackc = rb_define_module("RackC");
+  rb_define_singleton_method(rackc, "init!", method_rackc_init, 0);
+}
+
+VALUE method_rackc_init(VALUE self){
+  VALUE rack_arr;
+  VALUE headers;
+
+  rack_arr = rb_ary_new();
+  headers = rb_hash_new();
+  
+  rb_hash_aset(headers, (VALUE *)rb_str_new2("Content-Type"), (VALUE *)rb_str_new2("plain"));
+
+  rb_ary_push(rack_arr, INT2NUM(200)); //response code here
+  rb_ary_push(rack_arr, headers); //header hash here
+  rb_ary_push(rack_arr, (VALUE *)rb_str_new2("Hello World!")); //body here
+  
+  return rack_arr;
+}
